b2bcf624d8ff54eb9453ed5412eea18e6d24742a
Muhammad RIzki Amrinaldi <amrinaldi.rizki28@gmail.com>
2025-02-10 23:15:24 +0700

    fix

--- jadenCase.js
+++ jadenCase.js
@@ -1,6 +1,4 @@
 String.prototype.toJadenCase = function () {
-    console.log(this.split(" ").map((word) => word[0].toUpperCase() + word.slice(1)).join(" "))
-};
+    return this.split(" ").map((word) => word[0].toUpperCase() + word.slice(1)).join(" ")
+}
 
-
-console.log("How can mirrors be real if our eyes aren't real".toJadenCase())
